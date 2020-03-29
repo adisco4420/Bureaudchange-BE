bfb5ef6a556089650bc14db982ab386c5268fdc7
alabi sodiq <adisco4420@gmail.com>
2020-03-29 13:45:18 +0100

    update fund-wallet

--- src/environment/env.ts
+++ src/environment/env.ts
@@ -6,6 +6,7 @@ const env = {
     MAILGUN_KEY: process.env.MAILGUN_KEY,
     JWT_KEY: process.env.JWT_KEY,
     stripeTestKey: process.env.stripeTestKey,
-    currencyApiKey: process.env.currencyApiKey
+    currencyApiKey: process.env.currencyApiKey,
+    flutterwaveSecretHash: process.env.flutterwaveSecretHash
 }
 export default env;
