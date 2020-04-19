3c96bd0802abae4c6dc8c142f313b85b384ff342
Alabi Sodiq <adisco4420@gmail.com>
2020-04-19 13:06:03 +0100

    get user and all transaction , update transaction

--- src/controllers/transaction..control.ts
+++ src/controllers/transaction..control.ts
@@ -1,8 +1,11 @@
 import { Status } from './../dtos/enums/statusenums';
 import { BaseService } from "../services/base.service";
-import { NextFunction, Request, Response } from "express";
+import { Request, Response } from "express";
 import { TransI } from '../interfaces/interfaces';
 import TransModel from '../models/transaction.model';
+import { BasicResponse } from '../dtos/outputs/basicresponse';
+import WalletSrv from '../services/wallet.service';
+import WalletController from '../controllers/wallet.control';
 
 class TransactionController extends BaseService {
     Create(payload: TransI): Promise<any> {
@@ -17,9 +20,52 @@ class TransactionController extends BaseService {
         });
         return promise; 
     } 
-    Update() {
-        const data = {}
-        // this.Create({type: 'withdraw', status})
+    async Update(req: Request, res: Response) {
+        try {
+            let responseObj = null;
+            const {id, status} = req.params;
+            const trans: TransI = (await TransModel.findById(id)).toJSON();
+            if(trans) {
+                const {recieveCun, payCun, amount, userEmail: email} = trans;
+                switch (trans.type) {
+                    case 'deposit':
+                        console.log('deposit');
+                        break;
+                    case 'exchange':
+                        const transrate = await WalletSrv.TransRate({recieveCun, payCun});
+                        const total = Number(amount/transrate);
+                        const payload: any = {amount: total, currency: recieveCun, email }
+                        await WalletController.FundWallet(payload);
+                        const result = await TransModel.findByIdAndUpdate(id, {status: 'success'}, {new: true});
+                        responseObj = new BasicResponse(Status.SUCCESS, {data: result});
+                        break;
+                    default:
+                        break
+                }
+            } else {
+                responseObj = new BasicResponse(Status.NOT_FOUND, {msg: 'Transaction not found'})
+            }
+
+            this.sendResponse(responseObj, req, res);
+        } catch (error) {
+            this.sendResponse(new BasicResponse(Status.ERROR, error), req, res);  
+        }
+    }
+    async GetUserTrans(req: Request, res: Response) {
+        try {
+            const userTrans = await TransModel.find({userEmail: req.user.email}).sort({date: -1});
+            this.sendResponse(new BasicResponse(Status.SUCCESS, { msg: 'User Transactions', data: userTrans}), req, res);
+        } catch (error) {
+            this.sendResponse(new BasicResponse(Status.ERROR, error), req, res);
+        }
+    }
+    async GetAllTrans(req: Request, res: Response) {
+        try {
+            const allTrans = await TransModel.find({}).sort({date: -1});
+            this.sendResponse(new BasicResponse(Status.SUCCESS, { msg: 'All Transactions', data: allTrans}), req, res);
+        } catch (error) {
+            this.sendResponse(new BasicResponse(Status.ERROR, error), req, res);
+        }
     }
 }
 
